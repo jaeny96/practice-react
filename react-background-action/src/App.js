c7f3db7b34aac3e9bfbf0ecf5d03a9cd48d13d62
jisuOh <jisuoh1996@gmail.com>
2022-04-15 09:19:01 +0900

    feat : April 14
    4/14 study task

--- react-background-action/src/App.js
+++ react-background-action/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useState, useMemo } from "react";
+
+import Button from "./components/UI/Button/Button";
+import "./App.css";
+import DemoList from "./components/Demo/DemoList";
+// import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
+  // const [showParagraph, setShowParagraph] = useState(false);
+  // console.log("App RUNNING");
+  // const btnClickHandler = useCallback(() => {
+  //   setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  // }, []);
+  const [listTitle, setListTitle] = useState("My List");
+
+  const changeTitleHandler = useCallback(() => {
+    setListTitle("New Title");
+  }, []);
+
+  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/* <h1>Hi there!</h1> */}
+
+      <DemoList title={listTitle} items={listItems} />
+      {/* <DemoOutput show={showParagraph} /> */}
+      <Button onClick={changeTitleHandler}>Change List Title</Button>
+      {/* <Button onClick={btnClickHandler}>Toggle Paragraph!</Button> */}
     </div>
   );
 }
---
+++ react-background-action/src/components/Demo/DemoOutput.js
@@ -0,0 +1,10 @@
+import React from "react";
+import MyParagraph from "./MyParagraph";
+
+const DemoOutput = (props) => {
+  console.log("DemoOutput RUNNING");
+  // return <p>{props.show ? "This is new!" : ""}</p>;
+  return <MyParagraph>{props.show ? "This is new!" : ""}</MyParagraph>;
+};
+
+export default React.memo(DemoOutput);
