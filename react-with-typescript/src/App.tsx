2a2c9a083bf01fe1f9089d0ab116008b1fb7d896
jisuOh <jisuoh1996@gmail.com>
2022-04-28 09:47:49 +0900

    feat : April 28
    - finish react with typescript course (feat. Max in udemy)

--- react-with-typescript/src/App.tsx
+++ react-with-typescript/src/App.tsx
@@ -1,25 +1,13 @@
-import { useState } from "react";
 import Todos from "./components/Todos";
-import Todo from "./models/todo";
 import NewTodo from "./components/NewTodo";
+import TodosContextProvider from "./store/todos-context";
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
-  // [
-  //   new Todo("Learn React"),
-  //   new Todo("Learn TypeScript"),
-  // ];
-  const addTodoHandler = (text: string) => {
-    const newTodo = new Todo(text);
-    setTodos((prevTodos) => {
-      return prevTodos.concat(newTodo);
-    });
-  };
   return (
-    <div>
-      <NewTodo onAddTodo={addTodoHandler} />
-      <Todos items={todos} />
-    </div>
+    <TodosContextProvider>
+      <NewTodo />
+      <Todos />
+    </TodosContextProvider>
   );
 }
 
